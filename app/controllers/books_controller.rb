dbe6c5661e884cecd6bcf5defb32f4a8bd8de78e
EC2 Default User <ec2-user@ip-172-31-10-75.ec2.internal>
2022-03-05 16:14:51 +0000

    bookers2 debug

---
+++ app/controllers/books_controller.rb
@@ -0,0 +1,63 @@
+class BooksController < ApplicationController
+ before_action :authenticate_user!
+  def show
+    @book = Book.find(params[:id])
+    @user=User.find(params[:id])
+  end
+
+  def index
+    @books = Book.all
+    @book=Book.new
+    @user=current_user
+
+  end
+
+  def create
+    @books = Book.all
+    @book = Book.new(book_params)
+    @user=current_user
+    #@book.user.id=current_user.id
+    if @book.save
+      redirect_to book_path(@book.id), notice: "You have created book successfully."
+    else
+      flash.now[:alert]='error'
+      render 'index'
+    end
+  end
+
+  def edit
+    @book = Book.find(params[:id])
+  end
+
+  def update
+    @book = Book.find(params[:id])
+    if @book.update(book_params)
+      redirect_to book_path(@book), notice: "You have updated book successfully."
+    else
+      render "edit"
+    end
+  end
+
+  def delete
+    @book = Book.find(params[:id])
+    @book.destoy
+    redirect_to books_path
+  end
+
+  def destroy
+    book=Book.find(params[:id])
+    if book.destroy
+      redirect_to books_path
+    else
+      render "Show"
+    end
+  end
+
+  private
+
+  def book_params
+    # mergeを書いていないエラー
+    # アソシエーションの関係上、user_idもストロングパラメータに設定しないといけない
+    params.require(:book).permit(:title, :body).merge(user_id: current_user.id)
+  end
+end
